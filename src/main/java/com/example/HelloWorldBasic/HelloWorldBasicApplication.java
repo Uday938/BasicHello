69355c4a4c019ccb815572a895676c1bda4b886c
vishal <vraj99988@gmail.com>
2024-10-03 13:03:30 -0400

    First Commit

---
+++ src/main/java/com/example/HelloWorldBasic/HelloWorldBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldBasicApplication.class, args);
+	}
+
+}
